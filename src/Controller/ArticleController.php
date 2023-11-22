0300ce6361efd2c6235a7da9ed08ad8d0b1d4e9a
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2023-11-22 15:28:53 +0100

    articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Article;
 class ArticleController extends AbstractController
 {
     #[Route('/', name: 'app_article')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $en = $doctrine->getManager();
+
+        $articles = $en->getRepository(Article::class)->findAll();
+
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articles,
         ]);
     }
+
+    #[Route('/article/edit/{id_article}', name: 'edit_article')]
+    public function editArticle($id_article): Response
+    {
+       return $this->render('article/index.html.twig');
+    }
+
+    #[Route('/article/{id_article}', name: 'view_article')]
+    public function viewArticle($id_article): Response
+    {
+        return $this->render('article/index.html.twig');
+       
+    }
+  
+  
 }
