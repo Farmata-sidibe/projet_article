857bfe6036ed32ed3c4ca2f51a6ce2c0d9f9161d
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2023-11-22 17:28:10 +0100

    add article and view

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,4 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-alert("hello");
 //start the stimules application
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,11 +5,16 @@ namespace App\Controller;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use App\Form\ArticleType;
+
+
 class ArticleController extends AbstractController
 {
-    #[Route('/', name: 'app_article')]
+    #[Route('/', name: 'list_article')]
     public function index(ManagerRegistry $doctrine): Response
     {
         $en = $doctrine->getManager();
@@ -28,9 +33,40 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/article/{id_article}', name: 'view_article')]
-    public function viewArticle($id_article): Response
+    public function viewArticle($id_article,ManagerRegistry $doctrine): Response
     {
-        return $this->render('article/index.html.twig');
+        $en = $doctrine->getManager();
+        $article = $en->getRepository(Article::class)->find($id_article);
+
+        return $this->render('article/viewArticle.html.twig', [
+            'article' => $article,
+        ]);
+       
+    }
+    #[Route('/article-add', name: 'add_article')]
+    public function addArticle(Request $request,ManagerRegistry $doctrine): Response
+    {
+        $en = $doctrine->getManager();
+
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $article = $form->getData();
+
+            $article->setBody(nl2br($article->getBody()));
+
+            $en->persist($article);
+            $en->flush();
+
+            return $this->redirectToRoute('list_article');
+        }
+
+        return $this->render('article/addArticle.html.twig', [
+            'form' => $form->createView()
+        ]);
        
     }
   
